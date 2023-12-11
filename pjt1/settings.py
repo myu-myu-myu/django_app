88b4b0b6ba60522e1cf629a10662e9cd1b8e6243
myu <myu_uehara@mail.toyota.co.jp>
2023-12-11 13:00:44 +0900

    reset env

--- pjt1/settings.py
+++ pjt1/settings.py
@@ -31,11 +31,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
-    "crispy_bootstrap4",
+    # 'crispy_forms',
+    # "crispy_bootstrap4",
     'pjt1.app1',
 ]
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+# CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
